23d2b7c20a2ef51940cbc1f97e070c9b24e582ad
Vinay <vinay86bhamre@gmail.com>
2023-11-21 15:38:15 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import QueryProvider from "./lib/react-query/QueryProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>,
+);
